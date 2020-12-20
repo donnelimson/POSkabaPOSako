d538723acb64bd4f97442e87462a946ae35a286a
donnel imson <donnelantoinette24@yahoo.com>
2020-12-20 18:42:30 +0800

    Initialization

---
+++ POSkabaPOSako/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POSkabaPOSako
+{
+    public partial class Form1 : BaseController
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ConnectDatabase();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         //  MessageBox.Show("Database Connected");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
