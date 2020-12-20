fd8f7af1ada3acb5e1c7dab02f0be47496950cc2
donnel imson <donnelantoinette24@yahoo.com>
2020-12-20 23:07:58 +0800

    Code first migration

---
+++ Context/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Context.Migrations
+{
+    internal sealed class Configuration:DbMigrationsConfiguration<CommonContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+        protected override void Seed(CommonContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
